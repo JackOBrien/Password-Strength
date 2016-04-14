23fac0fe5a65f40d214ec36d3ece9d1dd6bc7ca4
Jack O'Brien <obrienjo@mail.gvsu.edu>
2016-04-14 15:50:21 -0400

    Implemented time estimate and simple color change

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,10 +1,25 @@
 package model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Model {
 
     private String password;
     private int characterSet;
     private double calcsPerSecond;
+
+    private final int LOWER_CHARSET = 26;
+    private final int UPPER_CHARSET = 26;
+    private final int NUMBER_CHARSET = 10;
+    private final int SYMBOL_CHARSET = 35;
+
+    private final String PASSWORD_FILE = "src/model/10k_most_common.txt";
 
     public Model(String password) {
         this.password = password;
@@ -12,9 +27,25 @@ public class Model {
         calcsPerSecond = 4000000000f;
     }
 
-    public double getCrackTime() {
+    public BigDecimal getCrackTime() throws NumberFormatException {
 
-        return (password.length() * characterSet) / calcsPerSecond;
+        if (hasLowerChar()) {
+            characterSet += LOWER_CHARSET;
+        }
+
+        if (hasUpperChar()) {
+            characterSet += UPPER_CHARSET;
+        }
+
+        if (hasNumber()) {
+            characterSet += NUMBER_CHARSET;
+        }
+
+        if (hasSymbol()) {
+            characterSet += SYMBOL_CHARSET;
+        }
+
+        return new BigDecimal(Math.pow(characterSet, password.length())/calcsPerSecond);
     }
 
     public boolean hasLowerChar() {
@@ -33,4 +64,95 @@ public class Model {
         return !password.matches("[A-Za-z0-9 ]*");
     }
 
+    private boolean searchFile() throws FileNotFoundException {
+        File file = new File(PASSWORD_FILE);
+        final Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            final String lineFromFile = scanner.nextLine();
+            if(lineFromFile.equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public String prettyCrackTime() {
+
+        if (password.length() < 1) return "Enter Password";
+
+        boolean inFile = false;
+
+        try {
+            inFile = searchFile();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (inFile) {
+            return "Instantly";
+        }
+
+        BigDecimal time = null;
+
+        try {
+            time = getCrackTime();
+        } catch (NumberFormatException e) {
+            return "Infinite";
+        }
+
+        String unit;
+
+        if (time.compareTo(new BigDecimal(0.000999)) <= 0) {
+            time = time.multiply(new BigDecimal(1000));
+            time = time.setScale(6, BigDecimal.ROUND_HALF_UP);
+            unit = " millisecond";
+        }
+
+        else if (time.compareTo(new BigDecimal(3600)) < 0) {
+            time = time.setScale(6, BigDecimal.ROUND_HALF_UP);
+            unit = " seconds";
+        }
+
+        else if (time.compareTo(new BigDecimal(86400)) < 0) {
+            BigDecimal divisor = new BigDecimal(3600.0);
+            time = time.divide(divisor, 6, BigDecimal.ROUND_HALF_UP);
+            time = time.setScale(6, BigDecimal.ROUND_HALF_UP);
+            unit = " hours";
+        }
+
+        else if (time.compareTo(new BigDecimal(20160 * 60)) <= 0) {
+            time = time.divide(new BigDecimal(86400.0), 2, BigDecimal.ROUND_HALF_UP);
+            time = time.setScale(2, BigDecimal.ROUND_HALF_UP);
+            unit = " days";
+        }
+
+        else if (time.compareTo(new BigDecimal(3.145 * Math.pow(10, 7))) <= 0) {
+            time = time.divide(new BigDecimal(604800), 2, BigDecimal.ROUND_HALF_UP);
+            time = time.setScale(2, BigDecimal.ROUND_HALF_UP);
+            unit = " weeks";
+        }
+
+        else {
+            BigDecimal divisor = new BigDecimal(3.145 * Math.pow(10, 7));
+            time = time.divide(divisor, 2, BigDecimal.ROUND_HALF_UP);
+            time = time.setScale(2, BigDecimal.ROUND_HALF_UP);
+            String str = NumberFormat.getNumberInstance().format(time.setScale(2, BigDecimal.ROUND_HALF_UP));
+//            String str = String.format("%,d", time.setScale(2, BigDecimal.ROUND_HALF_UP));
+            unit = " years";
+        }
+
+        NumberFormat df = DecimalFormat.getNumberInstance();
+        df.setMaximumFractionDigits(6);
+        df.setRoundingMode(RoundingMode.HALF_UP);
+
+        String formatted = "";
+
+        try {
+            formatted = df.format(time);
+        } catch (NumberFormatException e) {
+            return "Infinite";
+        }
+        return formatted + unit;
+    }
 }
--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.math.BigDecimal;
 
 public class UserInterface extends JFrame {
 
@@ -22,12 +23,17 @@ public class UserInterface extends JFrame {
     private final String X = "[✖]";
     private final String CHECK = "[✔]";
 
-    private final Color RED = Color.RED;
-    private final Color GREEN = new Color(37, 158, 50);
+    private final Color RED_FG = Color.RED;
+    private final Color GREEN_FG = new Color(37, 158, 50);
+
+    private final Color BLUE_BG = new Color(166, 194, 194);
+    private final Color RED_BG = new Color(225, 134, 116);
+    private final Color GREEN_BG = new Color(149, 194, 138);
 
     public UserInterface() {
-//        this.setPreferredSize(new Dimension(700, 500));
+        this.setPreferredSize(new Dimension(500, 220));
         this.setTitle("Password Strength Tester");
+
 
         setupPanel();
 
@@ -38,35 +44,50 @@ public class UserInterface extends JFrame {
 
     private void setupPanel() {
         mainPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
+        mainPanel.setBackground(BLUE_BG);
 
-//        passwordField = new JPasswordField();
-        passwordField = new JTextField();
+        passwordField = new JPasswordField();
+        //passwordField = new JTextField();
 
-        passwordField.setColumns(25);
+
+        passwordField.setColumns(35);
+        passwordField.setMinimumSize(new Dimension(300, 20));
         passwordField.addKeyListener(passwordListener);
 
         JPanel timePanel = new JPanel(new BorderLayout());
-        timePanel.setPreferredSize(new Dimension(400, 30));
-        crackTime = new JLabel("Test Text");
+        timePanel.setOpaque(false);
+//        timePanel.setPreferredSize(new Dimension(500, 40));
+        crackTime = new JLabel("Test Text", JLabel.CENTER);
         timePanel.add(crackTime, BorderLayout.CENTER);
 
+        JLabel title = new JLabel("How Strong is Your Password?", JLabel.CENTER);
+        Font font = new Font("SansSerif", Font.ITALIC | Font.BOLD, 22);
+        title.setFont(font);
+
         // -- Add components to the main panel --
+        GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 0;
+        c.gridwidth = 3;
+        c.anchor = GridBagConstraints.CENTER;
+        mainPanel.add(title, c);
+
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 1;
         c.gridwidth = 3;
         c.anchor = GridBagConstraints.CENTER;
         mainPanel.add(passwordField, c);
 
         c = new GridBagConstraints();
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 3;
         c.anchor = GridBagConstraints.LINE_START;
         mainPanel.add(createReqsPanel(), c);
 
         c = new GridBagConstraints();
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 2;
         c.gridwidth = 3;
         c.anchor = GridBagConstraints.CENTER;
         mainPanel.add(timePanel, c);
@@ -76,19 +97,20 @@ public class UserInterface extends JFrame {
 
     private JPanel createReqsPanel() {
         JPanel requirementsPanel = new JPanel();
+        requirementsPanel.setOpaque(false);
         requirementsPanel.setLayout(new BoxLayout(requirementsPanel, BoxLayout.Y_AXIS));
 
         hasLowerLabel = new JLabel(X + " No Lower Case");
-        hasLowerLabel.setForeground(RED);
+        hasLowerLabel.setForeground(RED_FG);
 
         hasUpperLabel = new JLabel(X + " No Upper Case");
-        hasUpperLabel.setForeground(RED);
+        hasUpperLabel.setForeground(RED_FG);
 
         hasNumberLabel = new JLabel(X + " No Number");
-        hasNumberLabel.setForeground(RED);
+        hasNumberLabel.setForeground(RED_FG);
 
         hasSymbol = new JLabel(X + " No Special Characters");
-        hasSymbol.setForeground(RED);
+        hasSymbol.setForeground(RED_FG);
 
         requirementsPanel.add(hasLowerLabel);
         requirementsPanel.add(hasUpperLabel);
@@ -112,35 +134,48 @@ public class UserInterface extends JFrame {
 
             if (model.hasLowerChar()) {
                 hasLowerLabel.setText(CHECK + " Has Lower Case");
-                hasLowerLabel.setForeground(GREEN);
+                hasLowerLabel.setForeground(GREEN_FG);
             } else {
                 hasLowerLabel.setText(X + " No Lower Case");
-                hasLowerLabel.setForeground(RED);
+                hasLowerLabel.setForeground(RED_FG);
             }
 
             if (model.hasUpperChar()) {
                 hasUpperLabel.setText(CHECK + " Has Upper Case");
-                hasUpperLabel.setForeground(GREEN);
+                hasUpperLabel.setForeground(GREEN_FG);
             } else {
                 hasUpperLabel.setText(X + " No Upper Case");
-                hasUpperLabel.setForeground(RED);
+                hasUpperLabel.setForeground(RED_FG);
             }
 
             if (model.hasNumber()) {
                 hasNumberLabel.setText(CHECK + " Has Number");
-                hasNumberLabel.setForeground(GREEN);
+                hasNumberLabel.setForeground(GREEN_FG);
             } else {
                 hasNumberLabel.setText(X + " No Number");
-                hasNumberLabel.setForeground(RED);
+                hasNumberLabel.setForeground(RED_FG);
             }
 
             if (model.hasSymbol()) {
                 hasSymbol.setText(CHECK + " Has Special Character");
-                hasSymbol.setForeground(GREEN);
+                hasSymbol.setForeground(GREEN_FG);
             } else {
                 hasSymbol.setText(X + " No Special Characters");
-                hasSymbol.setForeground(RED);
+                hasSymbol.setForeground(RED_FG);
             }
+
+            String text = model.prettyCrackTime();
+
+            if (text.endsWith("years")) {
+                mainPanel.setBackground(GREEN_BG);
+            } else if (!passwordField.getText().isEmpty()) {
+                mainPanel.setBackground(RED_BG);
+            } else {
+                mainPanel.setBackground(BLUE_BG);
+            }
+
+//            crackTime.setText(model.getCrackTime() + " seconds");
+            crackTime.setText(model.prettyCrackTime());
         }
     };
 }
